788a84383693ae735e7fcb37dd6d284c3287d78e
televizyonist <72473331+televizyonist@users.noreply.github.com>
2025-09-21 02:55:36 +0300

    Fix SlotRelationshipDisplay pointer enter braces

--- Assets/Scripts/SlotRelationshipDisplay.cs
+++ Assets/Scripts/SlotRelationshipDisplay.cs
@@ -311,7 +311,6 @@ public class SlotRelationshipDisplay : MonoBehaviour
             UpdateConnections();
             return;
         }
-    }
 
         if (_draggedCardDefinition == null)
         {
