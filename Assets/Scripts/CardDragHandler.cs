7856a5fc65836d9dcde963c5f748275854596767
televizyonist <72473331+televizyonist@users.noreply.github.com>
2025-09-20 19:58:31 +0300

    Fix vector addition during card drag

---
+++ Assets/Scripts/CardDragHandler.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class CardDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("Dependencies")]
+    [Tooltip("Canvas used to position the card while it is being dragged. Defaults to the first parent canvas if not assigned.")]
+    public Canvas dragCanvas;
+
+    [Header("Behaviour")]
+    [Tooltip("If enabled the card returns to its previous slot when it is not dropped on a valid slot.")]
+    public bool returnToOriginalSlotIfRejected = true;
+
+    private RectTransform _rectTransform;
+    private CanvasGroup _canvasGroup;
+    private Vector3 _dragOffset;
+
+    private Transform _originalParent;
+    private int _originalSiblingIndex;
+    private Vector2 _originalAnchoredPosition;
+    private CardSlot _originalSlot;
+    private CardSlot _currentSlot;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _canvasGroup = GetComponent<CanvasGroup>();
+        if (_canvasGroup == null)
+        {
+            _canvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+
+        if (dragCanvas == null)
+        {
+            dragCanvas = GetComponentInParent<Canvas>();
+        }
+
+        _currentSlot = GetComponentInParent<CardSlot>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _originalParent = _rectTransform.parent;
+        _originalSiblingIndex = _rectTransform.GetSiblingIndex();
+        _originalAnchoredPosition = _rectTransform.anchoredPosition;
+        _originalSlot = _currentSlot;
+
+        Vector3 pointerPosition = new Vector3(eventData.position.x, eventData.position.y, _rectTransform.position.z);
+        _dragOffset = _rectTransform.position - pointerPosition;
+
+        _canvasGroup.blocksRaycasts = false;
+
+        if (dragCanvas != null)
+        {
+            _rectTransform.SetParent(dragCanvas.transform, true);
+            _rectTransform.SetAsLastSibling();
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 pointerPosition = new Vector3(eventData.position.x, eventData.position.y, _rectTransform.position.z);
+        _rectTransform.position = pointerPosition + _dragOffset;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.blocksRaycasts = true;
+
+        CardSlot targetSlot = null;
+        if (eventData.pointerEnter != null)
+        {
+            targetSlot = eventData.pointerEnter.GetComponentInParent<CardSlot>();
+        }
+
+        if (targetSlot != null && targetSlot.TryAccept(this))
+        {
+            return;
+        }
+
+        if (returnToOriginalSlotIfRejected)
+        {
+            RestoreOriginalPlacement();
+        }
+    }
+
+    public void RestoreOriginalPlacement()
+    {
+        _currentSlot = _originalSlot;
+
+        if (_originalParent != null)
+        {
+            _rectTransform.SetParent(_originalParent, false);
+            _rectTransform.SetSiblingIndex(_originalSiblingIndex);
+            _rectTransform.anchoredPosition = _originalAnchoredPosition;
+        }
+    }
+
+    public void CompleteSlotDrop(CardSlot slot, Transform parent, Vector2 anchoredPosition)
+    {
+        _currentSlot = slot;
+
+        if (parent != null)
+        {
+            _rectTransform.SetParent(parent, false);
+        }
+
+        _rectTransform.SetAsLastSibling();
+        _rectTransform.anchoredPosition = anchoredPosition;
+    }
+
+    public RectTransform RectTransform => _rectTransform;
+}
